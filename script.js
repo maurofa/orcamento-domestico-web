99ad8a3f11109e029fbe7d0fed576738809e974d
Mauro Farias Brito <maurofabri@gmail.com>
2023-08-27 08:33:24 -0300

    script

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const path = 'http://127.0.0.1:5000';
 /*
  Função para obter o orçamento mensal ou anual
 */
-const getOrcamento = async (mes, ano) => {
+const getOrcamento = (mes, ano) => {
   let url = path + '/orcamento';
   if (!!ano) {
     url = url + '?ano=' + ano;
@@ -19,6 +19,67 @@ const getOrcamento = async (mes, ano) => {
 
 getOrcamento();
 
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para criar um botão close para cada item da lista
+  --------------------------------------------------------------------------------------
+*/
+const insertButton = (parent) => {
+  const span = document.createElement("span");
+  const txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  parent.appendChild(span);
+}
+
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para remover um item da lista de acordo com o click no botão close
+  --------------------------------------------------------------------------------------
+*/
+const removeElement = () => {
+  let close = document.getElementsByClassName("close");
+  // var table = document.getElementById('myTable');
+  let i;
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      let div = this.parentElement.parentElement;
+      const idLancamento = div.getElementsByTagName('td')[0].innerHTML
+      if (confirm("Você tem certeza?")) {
+        div.remove()
+        deleteItem(idLancamento)
+        alert("Removido!")
+      }
+    }
+  }
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para deletar um item da lista do servidor via requisição DELETE
+  --------------------------------------------------------------------------------------
+*/
+const deleteItem = (idLancamento) => {
+  console.log(idLancamento)
+  let url = `${path}/${idLancamento}/excluir`;
+  fetch(url, {
+    method: 'delete'
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para inserir items na lista apresentada
+  --------------------------------------------------------------------------------------
+*/
 function insertList(lancamento) {
   let item = [lancamento.id, lancamento.dataDoFato, lancamento.descricao, lancamento.valor, lancamento.ehReceita, lancamento.subGrupo];
   let tabela = document.getElementById('minhaTabela');
@@ -29,6 +90,7 @@ function insertList(lancamento) {
     cel.textContent = item[i];
   }
   insertButton(row.insertCell(-1));
-  document.getElementById("")
+
+  remove
 }
 
