14b4ed5e8dd8710f03c6e5f7f8f7f1e66cba7e66
Mauro Farias Brito <maurofabri@gmail.com>
2023-09-08 01:13:12 -0300

    alterações para atualizar o orçamento depois que deleta um lançamento.

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const path = 'http://127.0.0.1:5000';
 
-/*
- Função para obter o orçamento mensal ou anual
-*/
+let orcamento = [];
+const tabelaOrcamento = document.getElementById('minhaTabela');
+const form = document.getElementById('formCadastroLancamento');
+/**
+ * Função para obter o orçamento mensal ou anual
+ * @param {number?} mes
+ * @param {number?} ano
+ */
 const getOrcamento = (mes, ano) => {
   let url = path + '/gerar_orcamento';
   if (!!ano) {
@@ -14,21 +19,41 @@ const getOrcamento = (mes, ano) => {
   fetch(url, { method: 'GET' })
     .then((response) => response.json())
     .then((data) => {
-      ordenaOrcamento(data.orcamento).forEach(lancamento => insertList(lancamento));
-      const rodape = document.getElementById('Total');
-      rodape.textContent = `Quantidade de lançamentos: ${data.quantidade} Saldo: R$ ${data.saldo}`;
-      removeElement();
+      orcamento = data.orcamento;
+      const tbody = document.createElement('tbody');
+      tabelaOrcamento.appendChild(tbody);
+      carregaTabelaOrcamento(tbody);
+      addEventoBotaoFechar();
     })
     .catch((error) => console.error('Error:', error));
 }
 
 getOrcamento();
 
-/*
-  Ordena uma cópia do orçamento pela data e por receita/despesa (receita primeiro).
+/**
+ * Carrega o orçamento dentro do elemento do DOM enviado
+ * @param {HTMLTableSectionElement} tbody
+ */
+const carregaTabelaOrcamento = (tbody) => {
+  ordenaOrcamento(orcamento)
+    .map((lancamento, index, array) => {
+      const parcela = lancamento.valor * (lancamento.ehReceita ? 1 : -1);
+      if (!index) {
+        lancamento.saldo = parcela;
+      } else {
+        const anterior = array[index - 1];
+        lancamento.saldo = +(anterior.saldo + parcela).toFixed(2);
+      }
+      return lancamento;
+    })
+    .forEach(lancamento => insertList(lancamento, tbody));
+}
 
-  Retorna esta cópia.
-*/
+/**
+ * Ordena uma cópia do orçamento pela data e por receita/despesa (receita primeiro).
+ * @param {any[]} orcamento lista de lançamentos
+ * @returns Retorna esta cópia.
+ */
 const ordenaOrcamento = (orcamento) => {
   let copia = [...orcamento];
   copia.sort((a, b) => {
@@ -42,10 +67,8 @@ const ordenaOrcamento = (orcamento) => {
 }
 
 
-/*
-  --------------------------------------------------------------------------------------
-  Função para criar um botão close para cada item da lista
-  --------------------------------------------------------------------------------------
+/**
+  Função para criar um botão close
 */
 const insertButton = (parent) => {
   const button = document.createElement("button");
@@ -55,39 +78,39 @@ const insertButton = (parent) => {
 }
 
 
-/*
-  --------------------------------------------------------------------------------------
-  Função para remover um item da lista de acordo com o click no botão close
-  --------------------------------------------------------------------------------------
-*/
-const removeElement = () => {
+/**
+ * Função para adicionar os eventos dos botões de exclusão do lançamento de cada linha da tabela
+ */
+const addEventoBotaoFechar = () => {
   let close = document.getElementsByClassName("fecha-lancamento");
   let i;
   for (i = 0; i < close.length; i++) {
+    // cria um evento para click de cada botão close
     close[i].onclick = (ev) => {
       let div = ev.target.parentElement.parentElement;
       const idLancamento = div.getElementsByTagName('td')[0].innerHTML
       if (confirm("Você tem certeza que deseja excluir este lançamento?")) {
-        deleteItem(idLancamento, div)
+        deleteItem(idLancamento)
       }
     }
   }
 }
 
-/*
-  --------------------------------------------------------------------------------------
-  Função para deletar um item da lista do servidor via requisição DELETE
-  --------------------------------------------------------------------------------------
-*/
-const deleteItem = (idLancamento, div) => {
-  console.log(idLancamento)
+/**
+ * Função para deletar um lançamento da lista do servidor via requisição DELETE
+ * @param {number} idLancamento
+ */
+const deleteItem = (idLancamento) => {
   let url = `${path}/lancamento/${idLancamento}/excluir`;
-  let excluiu = false;
   fetch(url, {
     method: 'delete'
   })
     .then(() => {
-      div.remove();
+      orcamento = orcamento.filter(lancamento => lancamento.id != idLancamento);
+      const tbody = document.createElement('tbody');
+      carregaTabelaOrcamento(tbody);
+      const oldTbody = tabelaOrcamento.lastChild;
+      tabelaOrcamento.replaceChild(tbody, oldTbody);
       alert(`Lançamento de id ${idLancamento} removido!`);
     })
     .catch((error) => {
@@ -97,16 +120,15 @@ const deleteItem = (idLancamento, div) => {
 
 
 
-/*
-  --------------------------------------------------------------------------------------
-  Função para inserir items na lista apresentada
-  --------------------------------------------------------------------------------------
-*/
-const tabelaOrcamento = document.getElementById('minhaTabela');
-function insertList(lancamento) {
-  let dataDoFato = new Date(lancamento.dataDoFato+' 00:00:00').toLocaleDateString();
-  let item = [lancamento.id, dataDoFato, lancamento.descricao, lancamento.valor, lancamento.ehReceita, lancamento.subGrupo.descricao];
-  let linha = tabelaOrcamento.insertRow();
+/**
+ * Função para inserir lancamento na tabela, onde cada lançamento se tornará uma linha
+ * @param {*} lancamento
+ * @param {HTMLTableSectionElement} tbody
+ */
+function insertList(lancamento, tbody) {
+  let dataDoFato = new Date(lancamento.dataDoFato + ' 00:00:00').toLocaleDateString();
+  let item = [lancamento.id, dataDoFato, lancamento.descricao, lancamento.valor, lancamento.ehReceita, lancamento.subGrupo.descricao, lancamento.saldo];
+  let linha = tbody.insertRow();
   linha.className = lancamento.ehReceita ? 'table-success' : 'table-danger';
 
   for (let i = 0; i < item.length; i++) {
@@ -116,11 +138,7 @@ function insertList(lancamento) {
   insertButton(linha.insertCell(-1));
 }
 
-tabelaOrcamento.onchange = (ev) => {
-  console.log(ev);
-}
-
-/*
+/**
   click no botão maisUm abre o modal. Aqui preenchemos o select dos grupos
 */
 document.getElementById('maisUm').onclick = () => {
@@ -131,22 +149,24 @@ document.getElementById('maisUm').onclick = () => {
       const selectGrupo = document.getElementById('select-grupo');
       data.grupos.forEach(grupo => insertOption(grupo, selectGrupo));
       selectGrupo.onchange = () => populaSubGrupos();
-    })
+    });
 }
 
-/*
-  Insere uma opção em um select. O objeto grupo tem que ter um id e uma descrição.
-*/
-const insertOption = (grupo, select) => {
+/**
+ * Insere uma opção em um select.
+ * @param {{id: number, descricao: string}} entidade tem que ter um id e uma descrição
+ * @param {HTMLElement} select referência para o select do DOM
+ */
+const insertOption = (entidade, select) => {
   const option = document.createElement('option');
-  option.textContent = grupo.descricao;
-  option.value = grupo.id;
+  option.textContent = entidade.descricao;
+  option.value = entidade.id;
   select.appendChild(option);
 }
 
-/*
-  Depois que escolher um grupo, vai popular os subgrupos.
-*/
+/**
+ * Depois que escolher um grupo, vai popular os subgrupos.
+ */
 const populaSubGrupos = ()  => {
   const idGrupo = document.getElementById('select-grupo').value;
   const url = `${path}/grupo/${idGrupo}/sub-grupos`;
@@ -160,10 +180,11 @@ const populaSubGrupos = ()  => {
 }
 
 
-/*
-  Cria um evento para o formulário de cadastro de Lançamento
-*/
-const form = document.getElementById('formCadastroLancamento');
+
+/**
+ * Cria um evento para o formulário de cadastro de Lançamento
+ * @param {*} event
+ */
 form.onsubmit = (event) => {
   if (!form.checkValidity()) {
     event.preventDefault();
@@ -174,9 +195,14 @@ form.onsubmit = (event) => {
   form.classList.add('was-validated');
 }
 
-/*
-
-*/
+/**
+ * Insere um lançamento usando o método POST
+ * @param {number} idSubGrupo
+ * @param {string} dataDoFato
+ * @param {string} descricao
+ * @param {number} valor
+ * @param {boolean} ehReceita
+ */
 const postLancamento = (idSubGrupo, dataDoFato, descricao, valor, ehReceita) => {
   const formData = new FormData();
   formData.append('subGrupoId', idSubGrupo);
@@ -191,6 +217,12 @@ const postLancamento = (idSubGrupo, dataDoFato, descricao, valor, ehReceita) => 
     body: formData,
   })
     .then(response => response.json())
-    .then(data => insertList(data.lancamento))
+    .then(data => {
+      orcamento = orcamento.push(data.lancamento);
+      const tbody = document.createElement('tbody');
+      carregaTabelaOrcamento(tbody);
+      const oldTbody = tabelaOrcamento.lastChild;
+      tabelaOrcamento.replaceChild(tbody, oldTbody);
+    })
     .catch(error => console.error('Error: ', error));
 }
